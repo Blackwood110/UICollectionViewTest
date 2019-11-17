f83b11ae0bbcf145a6aad1586f6b05993192aea7
Сашка <aleksandrdergelev@mail.ru>
2019-11-17 17:42:20 +0300

    Initial Commit

---
+++ UICollectionViewTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  UICollectionViewTest
+//
+//  Created by Александр Дергилёв on 17.11.2019.
+//  Copyright © 2019 Александр Дергилёв. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
